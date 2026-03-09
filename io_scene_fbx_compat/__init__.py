72a3a03cfbb844392ad1fdcbba71011a66c31764
Un-Drew <bandrayy@gmail.com>
2026-03-09 09:55:10 +0200

    Vanilla FBX addon v4.5.5
    Source: https://projects.blender.org/blender/blender/src/tag/v4.5.5/scripts/addons_core/io_scene_fbx

--- io_scene_fbx_compat/__init__.py
+++ io_scene_fbx_compat/__init__.py
@@ -5,8 +5,8 @@
 bl_info = {
     "name": "FBX format",
     "author": "Campbell Barton, Bastien Montagne, Jens Restemeier, @Mysteryem",
-    "version": (5, 12, 7),
-    "blender": (4, 2, 0),
+    "version": (5, 13, 1),
+    "blender": (4, 5, 0),
     "location": "File > Import-Export",
     "description": "FBX IO meshes, UVs, vertex colors, materials, textures, cameras, lamps and actions",
     "warning": "",
@@ -51,7 +51,10 @@ class ImportFBX(bpy.types.Operator, ImportHelper):
     bl_label = "Import FBX"
     bl_options = {'UNDO', 'PRESET'}
 
-    directory: StringProperty()
+    directory: StringProperty(
+        subtype='DIR_PATH',
+        options={'HIDDEN', 'SKIP_PRESET'},
+    )
 
     filename_ext = ".fbx"
     filter_glob: StringProperty(default="*.fbx", options={'HIDDEN'})
@@ -59,6 +62,7 @@ class ImportFBX(bpy.types.Operator, ImportHelper):
     files: CollectionProperty(
         name="File Path",
         type=bpy.types.OperatorFileListElement,
+        options={'HIDDEN', 'SKIP_PRESET'},
     )
 
     ui_tab: EnumProperty(
@@ -211,9 +215,8 @@ class ImportFBX(bpy.types.Operator, ImportHelper):
 
         if self.files:
             ret = {'CANCELLED'}
-            dirname = os.path.dirname(self.filepath)
             for file in self.files:
-                path = os.path.join(dirname, file.name)
+                path = os.path.join(self.directory, file.name)
                 if import_fbx.load(self, context, filepath=path, **keywords) == {'FINISHED'}:
                     ret = {'FINISHED'}
             return ret
@@ -391,6 +394,7 @@ class ExportFBX(bpy.types.Operator, ExportHelper):
         items=(('OFF', "Normals Only", "Export only normals instead of writing edge or face smoothing data"),
                ('FACE', "Face", "Write face smoothing"),
                ('EDGE', "Edge", "Write edge smoothing"),
+               ('SMOOTH_GROUP', "Smoothing Groups", "Write face smoothing groups"),
                ),
         description="Export smoothing information "
         "(prefer 'Normals Only' option if your target importer understand split normals)",
