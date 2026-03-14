845721ad40e0af9b6c0b897423b1c76c323d02f2
Un-Drew <bandrayy@gmail.com>
2026-03-14 12:28:16 +0200

    Added API conditions for 3.0.0's changes

--- io_scene_fbx_compat/fbx_api_compat.py
+++ io_scene_fbx_compat/fbx_api_compat.py
@@ -21,13 +21,32 @@ __author__ = "UnDrew"
 # Checks whether a bpy type (an RNA struct) has a property by name.
 # Source: https://blender.stackexchange.com/a/300562
 def bpy_type_has_prop(btype, propname):
+    # NOTE: Doing some_blender_type.bl_rna returns some sort of metadata bpy_struct for that type.
+    #       The exact same metadata is also accessible from instances of that type.
     for prop in btype.bl_rna.properties:
         if prop.identifier == propname:
             return True
     return False
 
+def bpy_type_has_func(btype, funcname):
+    # NOTE: hasattr, when used on a bpy type, will detect functions.
+    return hasattr(btype, funcname)
+
+def py_class_has_prop(pyclass, propname):
+    # NOTE: hasattr, when used on a python class, will detect properties (those tied to a getter and setter).
+    return hasattr(pyclass, propname)
+
+def py_class_has_func(pyclass, funcname):
+    # NOTE: hasattr, when used on a python class, will ALSO detect functions.
+    return hasattr(pyclass, funcname)
+
 """ Added in 2.91.0 """
 
 HAS_MESH_ATTRIBUTES = bpy_type_has_prop(bpy.types.Mesh, 'attributes')
 HAS_SUBSURF_BOUNDARY_SMOOTH = bpy_type_has_prop(bpy.types.SubsurfModifier, 'boundary_smooth')
-HAS_BSDF_EMISSION_STRENGTH = hasattr(bpy_extras.node_shader_utils.PrincipledBSDFWrapper, 'emission_strength')
+HAS_BSDF_EMISSION_STRENGTH = py_class_has_prop(bpy_extras.node_shader_utils.PrincipledBSDFWrapper, 'emission_strength')
+
+""" Added in 3.0.0 """
+
+HAS_REFACTORED_UI_DATA = bpy_type_has_func(bpy.types.bpy_struct, 'id_properties_ui')
+HAS_REFACTORED_VISIBLE_FLAGS = bpy_type_has_prop(bpy.types.Object, 'visible_shadow')
