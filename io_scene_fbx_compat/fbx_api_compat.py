1e3ff089270a4ecdd365d49db3598efe77c9faf2
Un-Drew <bandrayy@gmail.com>
2026-03-18 10:29:10 +0200

    Added sources for each versions API changes

--- io_scene_fbx_compat/fbx_api_compat.py
+++ io_scene_fbx_compat/fbx_api_compat.py
@@ -64,33 +64,62 @@ def py_class_has_func(pyclass, funcname):
     # NOTE: hasattr, when used on a python class, will ALSO detect functions.
     return hasattr(pyclass, funcname)
 
-""" Added in 2.91.0 """
+"""
+Added in 2.91.0
+Sources:
+    * https://developer.blender.org/docs/release_notes/2.91/python_api/#other-changes
+    * https://developer.blender.org/docs/release_notes/2.91/modeling/#subdivision-surfaces
+    * https://developer.blender.org/docs/release_notes/2.91/python_api/#compatibility
+"""
 
 HAS_MESH_ATTRIBUTES = bpy_type_has_prop(bpy.types.Mesh, 'attributes')
 HAS_VRTX_COLS_AS_ATTRS = HAS_MESH_ATTRIBUTES
 HAS_SUBSURF_BOUNDARY_SMOOTH = bpy_type_has_prop(bpy.types.SubsurfModifier, 'boundary_smooth')
 HAS_BSDF_EMISSION_STRENGTH = py_class_has_prop(bpy_extras.node_shader_utils.PrincipledBSDFWrapper, 'emission_strength')
 
-""" Added in 3.0.0 """
+"""
+Added in 3.0.0
+Sources:
+    * https://developer.blender.org/docs/release_notes/3.0/python_api/#idproperty-ui-data-api
+    * https://docs.blender.org/api/2.93/bpy.types.Object.html#bpy.types.Object.cycles_visibility
+    * https://docs.blender.org/api/3.0/bpy.types.Object.html#bpy.types.Object.visible_shadow
+"""
 
 HAS_REFACTORED_UI_DATA = bpy_type_has_func(bpy.types.bpy_struct, 'id_properties_ui')
 HAS_REFACTORED_VISIBLE_FLAGS = bpy_type_has_prop(bpy.types.Object, 'visible_shadow')
 
-""" Added in 3.1.0 """
+"""
+Added in 3.1.0
+Source: https://developer.blender.org/docs/release_notes/3.1/python_api/#other-additions
+"""
 
 HAS_VRTX_AND_PLGN_NORM_ARRAYS = bpy_type_has_prop(bpy.types.Mesh, 'vertex_normals')
 
-""" Added in 3.2.0 """
+"""
+Added in 3.2.0
+Sources:
+    * https://developer.blender.org/docs/release_notes/3.2/sculpt/#color-attributes
+    * https://docs.blender.org/api/3.2/change_log.html#bpy-types-mesh
+"""
 
 HAS_MESH_COL_ATTRS_PROP = bpy_type_has_prop(bpy.types.Mesh, 'color_attributes')  # Also when the UI for them changed
 
-""" Added in 3.4.0 """
+"""
+Added in 3.4.0
+Sources:
+    * https://docs.blender.org/api/3.4/change_log.html#bpy-types-bytecolorattributevalue
+    * https://docs.blender.org/api/3.4/change_log.html#bpy-types-floatcolorattributevalue
+    * https://developer.blender.org/docs/release_notes/3.4/python_api/#internal-mesh-format
+"""
 
 HAS_COL_ATTR_SRGB_PROP = HAS_VRTX_COLS_AS_ATTRS and bpy_type_has_prop(bpy.types.ByteColorAttributeValue, 'color_srgb')
 HAS_REFACTORED_EDGE_CREASES = bpy_type_has_prop(bpy.types.Mesh, 'edge_creases')  # added 'has_crease_edge' at same time
 HAS_MESH_ATTR_MATERIAL_INDEX = check_ver(3, 4, 0, 'beta')  # unsure how to check this more concretely...
 
-""" Added in 3.5.0 """
+"""
+Added in 3.5.0
+Source: https://developer.blender.org/docs/release_notes/3.5/python_api/#internal-mesh-format
+"""
 
 HAS_MESH_ATTR_POSITION = check_ver(3, 5, 0, 'beta')  # unsure how to check this more concretely...
 HAS_MESH_ATTR_SHARP_EDGE = check_ver(3, 5, 0, 'beta')  # unsure how to check this more concretely...
