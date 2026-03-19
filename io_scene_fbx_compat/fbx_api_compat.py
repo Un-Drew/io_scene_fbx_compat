d204f5e791334e6fce91c139f80b621f01391f78
Un-Drew <bandrayy@gmail.com>
2026-03-19 12:24:01 +0200

    API compat: Few refactors in preparation for 4.0.0

--- io_scene_fbx_compat/fbx_api_compat.py
+++ io_scene_fbx_compat/fbx_api_compat.py
@@ -47,26 +47,46 @@ def check_ver(min_major, min_minor, min_micro, min_cycle):
         return False
     return BL_VER_CYCLE >= cycle_to_num(min_cycle)
 
-# Checks whether a bpy type has a property by name.
+"""
+Functions that check a class's props/funcs by name. This script uses these to find out which APIs are available.
+
+Motivation:
+
+A naive way to check whether an API is available would be to rely on the current Blender version. This works fine for
+official Blender releases, but wouldn't always be correct on pre-release builds. It's not uncommon for a property to be
+changed or replaced in the middle of a release cycle, so to support this, a more concrete check is needed.
+
+Fortunately, both Python and Blender's RNA system allow you, from the class alone, to explicitly check for props/funcs.
+Doing this is much safer than relying on the version alone.
+
+Why stop here:
+
+A safer (and technically more correct) approach would've been to check the instances at runtime, using exceptions.
+However, doing this for every instance would risk slowing the addon down, which I didn't want to do.
+
+In contrast, checking the class guarantees me that the result will be consistent throughout the process's lifetime,
+which in turn allows me to cache the result here. Also, personally, I think it's more readable this way.
+"""
+
+# Checks whether a natively-defined class has the specified RNA property.
 # Adapted from: https://blender.stackexchange.com/a/300562
-def bpy_type_has_prop(btype, propname):
-    return propname in btype.bl_rna.properties
+def class_has_rna_prop(cla, propname):
+    return propname in cla.bl_rna.properties
 
-def bpy_type_prop_is_readonly(btype, propname):
-    # NOTE: For even more *whacky* info about a property, run: print(dir(btype.bl_rna.properties[propname]))
-    return btype.bl_rna.properties[propname].is_readonly
+# Checks whether a class's RNA property is readonly.
+# NOTE: For a full list of all of a propery's attributes, run: print(dir(cla.bl_rna.properties[propname]))
+def class_rna_prop_is_readonly(cla, propname):
+    return cla.bl_rna.properties[propname].is_readonly
 
-def bpy_type_has_func(btype, funcname):
-    # NOTE: hasattr, when used on a bpy type, will detect functions.
-    return hasattr(btype, funcname)
+# Checks whether a class has the specified python property (not to be confused with python attributes).
+# NOTE: Sometimes, this is also applicable to natively-defined classes, because they're partially defined in Python.
+#       See: https://projects.blender.org/blender/blender/src/tag/v5.0.0/scripts/modules/_bpy_types.py
+def class_has_py_prop(cla, propname):
+    return hasattr(cla, propname)
 
-def py_class_has_prop(pyclass, propname):
-    # NOTE: hasattr, when used on a python class, will detect properties (those tied to a getter and setter).
-    return hasattr(pyclass, propname)
-
-def py_class_has_func(pyclass, funcname):
-    # NOTE: hasattr, when used on a python class, will ALSO detect functions.
-    return hasattr(pyclass, funcname)
+# Checks whether a class has a function (either natively-defined or python-defined).
+def class_has_func(cla, funcname):
+    return hasattr(cla, funcname)
 
 """
 Added in 2.91.0
@@ -76,10 +96,10 @@ Sources:
     * https://developer.blender.org/docs/release_notes/2.91/python_api/#compatibility
 """
 
-HAS_MESH_ATTRIBUTES = bpy_type_has_prop(bpy.types.Mesh, 'attributes')
+HAS_MESH_ATTRIBUTES = class_has_rna_prop(bpy.types.Mesh, 'attributes')
 HAS_VRTX_COLS_AS_ATTRS = HAS_MESH_ATTRIBUTES
-HAS_SUBSURF_BOUNDARY_SMOOTH = bpy_type_has_prop(bpy.types.SubsurfModifier, 'boundary_smooth')
-HAS_BSDF_EMISSION_STRENGTH = py_class_has_prop(bpy_extras.node_shader_utils.PrincipledBSDFWrapper, 'emission_strength')
+HAS_SUBSURF_BOUNDARY_SMOOTH = class_has_rna_prop(bpy.types.SubsurfModifier, 'boundary_smooth')
+HAS_BSDF_EMISSION_STRENGTH = class_has_py_prop(bpy_extras.node_shader_utils.PrincipledBSDFWrapper, 'emission_strength')
 
 """
 Added in 3.0.0
@@ -89,15 +109,15 @@ Sources:
     * https://docs.blender.org/api/3.0/bpy.types.Object.html#bpy.types.Object.visible_shadow
 """
 
-HAS_REFACTORED_UI_DATA = bpy_type_has_func(bpy.types.bpy_struct, 'id_properties_ui')
-HAS_REFACTORED_VISIBLE_FLAGS = bpy_type_has_prop(bpy.types.Object, 'visible_shadow')
+HAS_REFACTORED_UI_DATA = class_has_func(bpy.types.bpy_struct, 'id_properties_ui')
+HAS_REFACTORED_VISIBLE_FLAGS = class_has_rna_prop(bpy.types.Object, 'visible_shadow')
 
 """
 Added in 3.1.0
 Source: https://developer.blender.org/docs/release_notes/3.1/python_api/#other-additions
 """
 
-HAS_VRTX_AND_PLGN_NORM_ARRAYS = bpy_type_has_prop(bpy.types.Mesh, 'vertex_normals')
+HAS_VRTX_AND_PLGN_NORM_ARRAYS = class_has_rna_prop(bpy.types.Mesh, 'vertex_normals')
 
 """
 Added in 3.2.0
@@ -106,7 +126,7 @@ Sources:
     * https://docs.blender.org/api/3.2/change_log.html#bpy-types-mesh
 """
 
-HAS_MESH_COL_ATTRS_PROP = bpy_type_has_prop(bpy.types.Mesh, 'color_attributes')  # Also when the UI for them changed
+HAS_MESH_COL_ATTRS_PROP = class_has_rna_prop(bpy.types.Mesh, 'color_attributes')  # Also when the UI for them changed
 
 """
 Added in 3.4.0
@@ -116,9 +136,10 @@ Sources:
     * https://developer.blender.org/docs/release_notes/3.4/python_api/#internal-mesh-format
 """
 
-HAS_COL_ATTR_SRGB_PROP = HAS_VRTX_COLS_AS_ATTRS and bpy_type_has_prop(bpy.types.ByteColorAttributeValue, 'color_srgb')
-HAS_REFACTORED_EDGE_CREASES = bpy_type_has_prop(bpy.types.Mesh, 'edge_creases')  # added 'has_crease_edge' at same time
+HAS_COL_ATTR_SRGB_PROP = HAS_VRTX_COLS_AS_ATTRS and class_has_rna_prop(bpy.types.ByteColorAttributeValue, 'color_srgb')
 HAS_MESH_ATTR_MATERIAL_INDEX = check_ver(3, 4, 0, 'beta')  # unsure how to check this more concretely...
+# NOTE: - This added `Mesh.has_crease_edge` at same time.
+HAS_REFACTORED_EDGE_CREASES = class_has_rna_prop(bpy.types.Mesh, 'edge_creases')
 
 """
 Added in 3.5.0
@@ -127,14 +148,14 @@ Source: https://developer.blender.org/docs/release_notes/3.5/python_api/#interna
 
 HAS_MESH_ATTR_POSITION = check_ver(3, 5, 0, 'beta')  # unsure how to check this more concretely...
 HAS_MESH_ATTR_SHARP_EDGE = check_ver(3, 5, 0, 'beta')  # unsure how to check this more concretely...
-HAS_UV_LAYER_UV_PROP = bpy_type_has_prop(bpy.types.MeshUVLoopLayer, 'uv')
+HAS_UV_LAYER_UV_PROP = class_has_rna_prop(bpy.types.MeshUVLoopLayer, 'uv')
 
 """
 Added in 3.6.0
 Source: https://developer.blender.org/docs/release_notes/3.6/python_api/#internal-mesh-format
 """
 
-HAS_REFACTORED_POLYS_FOR_CONSISTENT_ORDER_WITH_LOOPS = bpy_type_prop_is_readonly(bpy.types.MeshPolygon, 'loop_total')
+HAS_REFACTORED_POLYS_FOR_CONSISTENT_ORDER_WITH_LOOPS = class_rna_prop_is_readonly(bpy.types.MeshPolygon, 'loop_total')
 HAS_MESH_ATTR_SHARP_FACE = check_ver(3, 6, 0, 'beta')  # unsure how to check this more concretely...
 HAS_MESH_ATTRS_CORNER_VERT_AND_CORNER_EDGE = check_ver(3, 6, 0, 'beta')  # unsure how to check this more concretely...
 HAS_MESH_ATTR_EDGE_VERTS = check_ver(3, 6, 0, 'beta')  # unsure how to check this more concretely...
